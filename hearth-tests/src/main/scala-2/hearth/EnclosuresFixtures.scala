f2d4b399767a0a943273c773cfdba9b136e1e50e
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2026-06-13 14:25:16 +0200

    scalafmt: format Enclosures sources

--- hearth-tests/src/main/scala-2/hearth/EnclosuresFixtures.scala
+++ hearth-tests/src/main/scala-2/hearth/EnclosuresFixtures.scala
@@ -5,9 +5,7 @@ import hearth.data.Data
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
-final private class EnclosuresFixtures(val c: blackbox.Context)
-    extends MacroCommonsScala2
-    with EnclosuresFixturesImpl {
+final private class EnclosuresFixtures(val c: blackbox.Context) extends MacroCommonsScala2 with EnclosuresFixturesImpl {
 
   def testEnclosingScopeImpl: c.Expr[Data] = testEnclosingScope
   def testEnclosingScopeReachesPackageImpl: c.Expr[Data] = testEnclosingScopeReachesPackage
